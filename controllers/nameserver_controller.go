7a8a6a2d1dc98f82f1106949782f49c11f2f11e0
kairii <1124350308@qq.com>
2021-12-07 17:29:48 +0800

    以deployment的形式创建namesever，并且为创建的这个nameserver创建一个无头服务

--- controllers/nameserver_controller.go
+++ controllers/nameserver_controller.go
@@ -18,13 +18,17 @@ package controllers
 
 import (
 	"context"
-
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/utils/pointer"
+	"rocketmq-operator-v2/api/v1"
+	rocketmqv1 "rocketmq-operator-v2/api/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	rocketmqv1 "rocketmq-operator-v2/api/v1"
 )
 
 // NameserverReconciler reconciles a Nameserver object
@@ -32,14 +36,105 @@ type NameserverReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
+	v1.Nameserver
 }
 
 // +kubebuilder:rbac:groups=rocketmq.daocloud.io,resources=nameservers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=rocketmq.daocloud.io,resources=nameservers/status,verbs=get;update;patch
 
+//以deployment的形式创建namesever，并且为创建的这个nameserver创建一个无头服务
+
 func (r *NameserverReconciler) Reconcile(context context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = r.Log.WithValues("nameserver", req.NamespacedName)
 
+	var NameSever v1.Nameserver
+	if err := r.Get(context, req.NamespacedName, &NameSever); err != nil {
+		log.Error(err, "unable to fetch NameSever")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	deploy := &appsv1.Deployment{
+		TypeMeta:   NameSever.TypeMeta,
+		ObjectMeta: NameSever.ObjectMeta,
+		Spec: appsv1.DeploymentSpec{
+			Replicas: pointer.Int32Ptr(rune(r.Nameserver.Spec.NameserverNumber)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "rocketmq-nameSvr",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "nameSever",
+					Labels: map[string]string{
+						"app":     "rocketmq-nameSvr",
+						"version": "v1",
+					},
+				},
+				Spec: corev1.PodSpec{
+					HostAliases:      NameSever.Spec.PodSpec.HostAliases,
+					RestartPolicy:    NameSever.Spec.PodSpec.RestartPolicy,
+					NodeSelector:     NameSever.Spec.PodSpec.NodeSelector,
+					SecurityContext:  NameSever.Spec.PodSpec.SecurityContext,
+					Affinity:         NameSever.Spec.PodSpec.Affinity,
+					Tolerations:      NameSever.Spec.PodSpec.Tolerations,
+					ImagePullSecrets: NameSever.Spec.Image.ImagePullSecret,
+					Containers: []corev1.Container{
+						{
+							Name:            "rocketmq-nameSrv",
+							Image:           NameSever.Spec.Image.Image,
+							ImagePullPolicy: NameSever.Spec.Image.ImagePullPolicy,
+						},
+					},
+				},
+			},
+			MinReadySeconds:         0,
+			RevisionHistoryLimit:    pointer.Int32Ptr(10),
+			Paused:                  false,
+			ProgressDeadlineSeconds: pointer.Int32Ptr(600),
+		},
+	}
+
+	if err := r.Client.Create(context, deploy, &client.CreateOptions{DryRun: []string{"666"}}); err != nil {
+		log.Error(err, "unable to create deployment")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var NameSeverLists v1.NameserverList
+	if err := r.List(context, &NameSeverLists, client.InNamespace(req.Namespace), client.MatchingFields{"nameSever": req.Name}); err != nil {
+		log.Error(err, "unable to list nameSevers")
+		return ctrl.Result{}, err
+	}
+
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: NameSever.Spec.ServiceAccountName,
+			Labels: map[string]string{
+				"apps": "nameSrv-headless",
+			},
+			OwnerReferences: []metav1.OwnerReference{
+				*metav1.NewControllerRef(deploy, schema.GroupVersionKind{
+					Group: "rocketmq.daocloud.io",
+				}),
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Type:      corev1.ServiceTypeNodePort,
+			ClusterIP: "none",
+			Selector: map[string]string{
+				"app": "rocketmq-nameSvc",
+			},
+		},
+	}
+
+	if err := r.Client.Create(context, svc, &client.CreateOptions{DryRun: []string{"666"}}); err != nil {
+		log.Error(err, "unable to create service")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 	return ctrl.Result{}, nil
 }
 
